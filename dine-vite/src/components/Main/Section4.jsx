206a31f3f44545719ba458a3f9341a2202c69aa1
Dong Hee Lee <dhblee0618@gmail.com>
2025-03-08 14:03:10 +1100

    added

--- dine-vite/src/components/Main/Section4.jsx
+++ dine-vite/src/components/Main/Section4.jsx
@@ -12,6 +12,10 @@ import specialImgDesktop from '../../assets/images/homepage/special-events-deskt
 import socialImgMobile from '../../assets/images/homepage/social-events-mobile.jpg';
 import socialImgTablet from '../../assets/images/homepage/social-events-tablet.jpg';
 import socialImgDesktop from '../../assets/images/homepage/social-events-desktop.jpg';
+
+import patternRight from '../../assets/images/patterns/pattern-curve-top-right.svg';
+import patternLine from '../../assets/images/patterns/pattern-lines.svg';
+
 const information = [
   {
     imgMobile: familyImgMobile,
@@ -39,35 +43,41 @@ const Section4 = ({px}) => {
   const [details, setDetails] = useState(information[0])
 
   return (
-    <section className={`${px}`}>
-      <picture>
-              <source srcSet={details.imgDesktop} media="(min-width: 1440px)" />
-              
-              <source srcSet={details.imgTablet} media="(min-width: 768px)" />
-              
-              <img src={details.imgMobile} alt="Local background" className='relative z-10 w-full 1440:mb-[-5rem]'/>
-      </picture>
-      <div>
-        <ul>
+    <section className={`relative ${px} flex flex-col 1440:flex-row gap-[49px] md:gap-[57px] 1440:gap-[125px]`}>
+      <div className="relative">
+        <picture>
+                <source srcSet={details.imgDesktop} media="(min-width: 1440px)" />
+                
+                <source srcSet={details.imgTablet} media="(min-width: 768px)" />
+                
+                <img src={details.imgMobile} alt="Local background" className='relative z-10 w-full 1440:mb-[-5rem]'/>
+        </picture>
+        <img src={patternLine} alt="" className="hidden md:block absolute top-[-2rem] left-[-4rem] z-20"/>
+      </div>
+      <div className="flex flex-col gap-[27px] md:gap-[47px] 1440:gap-[79px] 1440:flex-col-reverse">
+        <ul className="flex flex-col md:flex-row md:justify-between 1440:flex-col items-center">
           {information.map((item, index) => (
               <li key={item.title}>
                 <button
                   onClick={() => setDetails(information[index])}
-                  className={`headingS text-ShuttleGray hover:text-Clay font-bold `} 
+                  className={`headingS font-bold ${details.title === item.title ? 'text-Clay' : 'text-ShuttleGray hover:text-Clay'} `} 
                 >
                   {item.title.toUpperCase()}
                 </button>
               </li>
             ))}
         </ul>
-        <div>
-          <h2>{details.title}</h2>
-          <p>{details.details}</p>
-          <Button>
+        <div className="grid gap-[27px] md:gap-[60px] text-center 1440:text-left ">
+          <div className="grid gap-[13px] md:gap-[19px]">
+            <h2 className="headingL">{details.title}</h2>
+            <p className="body2 md:body1 max-w-[40ch] mx-auto">{details.details}</p>
+          </div>
+          <Button classStyle="border-white text-white bg-black 1440:mx-0 ">
             BOOK A TABLE
           </Button>
         </div>
       </div>
+      <img src={patternRight} alt="" className="hidden md:block absolute top-[-6rem] left-[-7rem] w-[50%] h-[60%]" />
     </section>
   )
 }
